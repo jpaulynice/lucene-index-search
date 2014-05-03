8b1dd75351edaf02ccda6df33e944020e71ad78e
julesbond007 <jay.paulynice@gmail.com>
2014-05-03 19:32:37 -0400

    format code

--- src/com/search/indexer/SimpleFileIndexer.java
+++ src/com/search/indexer/SimpleFileIndexer.java
@@ -20,100 +20,101 @@ import org.apache.lucene.util.Version;
  * Simple file indexer class.
  * 
  * @author Jules Jay Paulynice
- *
+ * 
  */
 public class SimpleFileIndexer {
-    
+
 	/**
 	 * Main method
 	 * 
 	 * @param args
 	 * @throws Exception
 	 */
-    public static void main(String[] args) throws Exception {
-        
-    	Directory directory = FSDirectory.open(new File("/Users/julespaulynice/Documents/search/index"));
-        File dataDir = new File("/Users/julespaulynice/Documents/workspace");
-        String suffix = "java";
-        
-        SimpleFileIndexer indexer = new SimpleFileIndexer();
-        
-        int numIndex = indexer.index(directory, dataDir, suffix);
-        
-        System.out.println("Total files indexed " + numIndex);
-    }
-    
-    /**
-     * Method to index a directory by passing the directory to index, location to store the index, and word endings.
-     * 
-     * @param indexDir
-     * @param dataDir
-     * @param suffix
-     * @return
-     * @throws Exception
-     */
-    private int index(Directory indexDir, File dataDir, String suffix) throws Exception {
-      
-    	StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_47);
-    	IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_47, analyzer);
-    	IndexWriter indexWriter = new IndexWriter(indexDir, config);       
-        indexDirectory(indexWriter, dataDir, suffix);
-        
-        int numIndexed = indexWriter.maxDoc();
-        indexWriter.close();
-        
-        return numIndexed;
-        
-    }
-    
-    /**
-     * Method to index a directory recursively.
-     * 
-     * @param indexWriter
-     * @param dataDir
-     * @param suffix
-     * @throws IOException
-     */
-    private void indexDirectory(IndexWriter indexWriter, File dataDir, 
-           String suffix) throws IOException {
+	public static void main(String[] args) throws Exception {
 
-        File[] files = dataDir.listFiles();
-        for (int i = 0; i < files.length; i++) {
-            File f = files[i];
-            if (f.isDirectory()) {
-                indexDirectory(indexWriter, f, suffix);
-            }
-            else {
-                indexFileWithIndexWriter(indexWriter, f, suffix);
-            }
-        }
+		Directory directory = FSDirectory.open(new File("/Users/julespaulynice/Documents/search/index"));
+		File dataDir = new File("/Users/julespaulynice/Documents/workspace");
+		String suffix = "java";
 
-    }
-    
-    /**
-     * Index a file by creating a Document and adding fields
-     * 
-     * @param indexWriter
-     * @param f
-     * @param suffix
-     * @throws IOException
-     */
-    private void indexFileWithIndexWriter(IndexWriter indexWriter, File f, 
-            String suffix) throws IOException {
+		SimpleFileIndexer indexer = new SimpleFileIndexer();
 
-        if (f.isHidden() || f.isDirectory() || !f.canRead() || !f.exists()) {
-            return;
-        }
-        if (suffix!=null && !f.getName().endsWith(suffix)) {
-            return;
-        }
-        System.out.println("Indexing file " + f.getName());
-        
-        Document doc = new Document();
-        doc.add(new Field("contents", new FileReader(f),TextField.TYPE_NOT_STORED));        
-        doc.add(new StringField("filepath", f.getCanonicalPath(), Field.Store.YES));
-        doc.add(new StringField("filename", f.getName(), Field.Store.YES));
-        
-        indexWriter.addDocument(doc);
-    }
+		int numIndex = indexer.index(directory, dataDir, suffix);
+
+		System.out.println("Total files indexed " + numIndex);
+	}
+
+	/**
+	 * Method to index a directory by passing the directory to index, location
+	 * to store the index, and word endings.
+	 * 
+	 * @param indexDir
+	 * @param dataDir
+	 * @param suffix
+	 * @return
+	 * @throws Exception
+	 */
+	private int index(Directory indexDir, File dataDir, String suffix)
+			throws Exception {
+
+		StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_47);
+		IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_47,analyzer);
+		IndexWriter indexWriter = new IndexWriter(indexDir, config);
+		indexDirectory(indexWriter, dataDir, suffix);
+
+		int numIndexed = indexWriter.maxDoc();
+		indexWriter.close();
+
+		return numIndexed;
+
+	}
+
+	/**
+	 * Method to index a directory recursively.
+	 * 
+	 * @param indexWriter
+	 * @param dataDir
+	 * @param suffix
+	 * @throws IOException
+	 */
+	private void indexDirectory(IndexWriter indexWriter, File dataDir,
+			String suffix) throws IOException {
+
+		File[] files = dataDir.listFiles();
+		for (int i = 0; i < files.length; i++) {
+			File f = files[i];
+			if (f.isDirectory()) {
+				indexDirectory(indexWriter, f, suffix);
+			} else {
+				indexFileWithIndexWriter(indexWriter, f, suffix);
+			}
+		}
+
+	}
+
+	/**
+	 * Index a file by creating a Document and adding fields
+	 * 
+	 * @param indexWriter
+	 * @param f
+	 * @param suffix
+	 * @throws IOException
+	 */
+	private void indexFileWithIndexWriter(IndexWriter indexWriter, File f,
+			String suffix) throws IOException {
+
+		if (f.isHidden() || f.isDirectory() || !f.canRead() || !f.exists()) {
+			return;
+		}
+		if (suffix != null && !f.getName().endsWith(suffix)) {
+			return;
+		}
+		System.out.println("Indexing file " + f.getName());
+
+		Document doc = new Document();
+		doc.add(new Field("contents", new FileReader(f),TextField.TYPE_NOT_STORED));
+		doc.add(new StringField("filepath", f.getCanonicalPath(),Field.Store.YES));
+		doc.add(new StringField("filename", f.getName(), Field.Store.YES));
+
+		indexWriter.addDocument(doc);
+	}
 }
--- src/com/search/searcher/SimpleSearcher.java
+++ src/com/search/searcher/SimpleSearcher.java
@@ -17,52 +17,53 @@ import org.apache.lucene.util.Version;
  * Simple searcher class
  * 
  * @author Jules Jay Paulynice
- *
+ * 
  */
 public class SimpleSearcher {
-    
+
 	/**
 	 * Main method
 	 * 
 	 * @param args
 	 * @throws Exception
 	 */
-    public static void main(String[] args) throws Exception {
-        
-        File indexDir = new File("/Users/julespaulynice/Documents/search/index");
-    	Directory directory = FSDirectory.open(indexDir);
+	public static void main(String[] args) throws Exception {
 
-        String query = "java";
-        int hits = 100;
-        
-        SimpleSearcher searcher = new SimpleSearcher();
-        searcher.searchIndex(directory, query, hits);
-        
-    }
-    
-    /**
-     * Search a given index for the search term and return maxHits results.
-     * 
-     * @param indexDir
-     * @param queryStr
-     * @param maxHits
-     * @throws Exception
-     */
-    private void searchIndex(Directory indexDir, String queryStr, int maxHits) 
-            throws Exception {
-    	DirectoryReader ireader = DirectoryReader.open(indexDir);
-	    IndexSearcher searcher = new IndexSearcher(ireader);
-	    QueryParser parser = new QueryParser(Version.LUCENE_47, "contents", new StandardAnalyzer(Version.LUCENE_47));
-	    
-	    Query query = parser.parse(queryStr);
-	    ScoreDoc[] hits = searcher.search(query, null, maxHits).scoreDocs;
-        
-        for (int i = 0; i < hits.length; i++) {
-            int docId = hits[i].doc;
-            Document d = searcher.doc(docId);
-            System.out.println(d.get("filename"));
-        }
-        
-        System.out.println("Found " + hits.length);
-    }
+		File indexDir = new File("/Users/julespaulynice/Documents/search/index");
+		Directory directory = FSDirectory.open(indexDir);
+
+		String query = "java";
+		int hits = 100;
+
+		SimpleSearcher searcher = new SimpleSearcher();
+		searcher.searchIndex(directory, query, hits);
+
+	}
+
+	/**
+	 * Search a given index for the search term and return maxHits results.
+	 * 
+	 * @param indexDir
+	 * @param queryStr
+	 * @param maxHits
+	 * @throws Exception
+	 */
+	private void searchIndex(Directory indexDir, String queryStr, int maxHits)
+			throws Exception {
+		DirectoryReader ireader = DirectoryReader.open(indexDir);
+		IndexSearcher searcher = new IndexSearcher(ireader);
+		QueryParser parser = new QueryParser(Version.LUCENE_47, "contents",
+				new StandardAnalyzer(Version.LUCENE_47));
+
+		Query query = parser.parse(queryStr);
+		ScoreDoc[] hits = searcher.search(query, null, maxHits).scoreDocs;
+
+		for (int i = 0; i < hits.length; i++) {
+			int docId = hits[i].doc;
+			Document d = searcher.doc(docId);
+			System.out.println(d.get("filename"));
+		}
+
+		System.out.println("Found " + hits.length);
+	}
 }
